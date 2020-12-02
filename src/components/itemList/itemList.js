66d68decf50a1bffd7baf0c7039c1fd3488e05dd
newbie <newbie@newbies-MacBook-Pro.local>
2020-12-02 22:45:05 +0200

    css => styled component

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,19 +1,31 @@
 import React, {Component} from 'react';
-import './itemList.css';
+
+import styled from 'styled-components';
+
+const ListItem = styled.li`
+    cursor: pointer;
+    position: relative;
+    display: block;
+    padding: 0.75rem 1.25rem; 
+    background-color: #fff;
+    border: 1px solid rgba(0, 0, 0, 0.125);
+`
+
+
 export default class ItemList extends Component {
 
     render() {
         return (
             <ul className="item-list list-group">
-                <li className="list-group-item">
+                <ListItem>
                     John Snow
-                </li>
-                <li className="list-group-item">
+                </ListItem>
+                <ListItem>
                     Brandon Stark
-                </li>
-                <li className="list-group-item">
+                </ListItem>
+                <ListItem>
                     Geremy
-                </li>
+                </ListItem>
             </ul>
         );
     }
